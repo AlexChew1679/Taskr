e012e77f5ff34b27d0cbce6cf21429da5e94f442
Alex Chew <geminia79@gmail.com>
2017-05-09 10:41:41 +0800

    add bootstarp twitter

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  
   root 'pages#home'
+  get 'about' => 'pages#about'
+  # 'about is just a name, if you put name as heyhey, then localhost:3000/heyhey will take you to pages#about '
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
