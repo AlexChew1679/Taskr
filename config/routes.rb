3f084c91adde289e00353192ebf4f2f898ffbf79
Alex Chew <geminia79@gmail.com>
2017-05-11 14:45:27 +0800

    added flash msg, error msg, tasksController

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   root 'pages#home'
   get 'about' => 'pages#about'
+
+  resources :tasks
   # 'about is just a name, if you put name as heyhey, then localhost:3000/heyhey will take you to pages#about '
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
