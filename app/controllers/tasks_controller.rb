9b4523e146c0a720ae11cbac38db588108eb3e08
Alex Chew <geminia79@gmail.com>
2017-05-11 23:31:13 +0800

    added destroy, render controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :show, :destroy]
+
 
     def new
       @task = Task.new
@@ -15,12 +17,12 @@ class TasksController < ApplicationController
     end
 
     def edit
-        @task = Task.find(params[:id])
+        #@task = Task.find(params[:id])
 
     end
 
     def update
-        @task = Task.find(params[:id])
+        #@task = Task.find(params[:id])
         if @task.update(tasks_params)
           flash[:notice] = "Task was succesfully updatedd"
           redirect_to task_path(@task)
@@ -30,7 +32,7 @@ class TasksController < ApplicationController
     end
 
     def show
-      @task = Task.find(params[:id])
+      #@task = Task.find(params[:id])
     end
 
     def index
@@ -38,16 +40,21 @@ class TasksController < ApplicationController
     end
 
     def destroy
+      #@task = Task.find(params[:id])
       @task.destroy
       flash[:notice] = "Task was succesfully deleted"
       redirect_to tasks_path
-    end  
+    end
 
 
 
 
 
     private
+
+    def set_task
+        @task = Task.find(params[:id])
+      end
 
     def tasks_params
       params.require(:task).permit(:content)
