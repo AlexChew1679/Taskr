422ce8235ba1c1ec96a2c59e642470132a0eb4d4
Alex Chew <geminia79@gmail.com>
2017-05-13 01:05:14 +0800

    add delete X, add arrow,

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
 
   def home
+    if current_user
+      redirect_to tasks_path
+    end
   end
 
   def about
@@ -8,6 +11,6 @@ class PagesController < ApplicationController
 
   def test
   end
-  
+
 
 end
